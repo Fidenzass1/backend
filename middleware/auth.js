2e1db61b474448c1deda584e6edbcbb1abe8347e
Asry18 <asryamaz000@gmail.com>
2025-10-22 23:02:41 +0530

    routing

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-const jwt = require('express-jwt');
+const { expressjwt: jwt } = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
 const checkJwt = jwt({
@@ -8,7 +8,7 @@ const checkJwt = jwt({
     jwksRequestsPerMinute: 5,
     jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
   }),
-  audience: process.env.AUTH0_AUDIENCE, // only if API identifier set
+  audience: process.env.AUTH0_AUDIENCE,
   issuer: `https://${process.env.AUTH0_DOMAIN}/`,
   algorithms: ['RS256'],
 });
